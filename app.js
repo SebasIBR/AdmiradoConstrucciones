b5f75a849b7b09de4b0588e67007ff38a93e1247
SebasIBR <negraoo477@gmail.com>
2023-02-22 16:08:48 -0500

    mainbar carrousel y adelanto del main

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ const app = express();
 const path = require('path')
 
 app.get("/",(req,res) => {
-    res.sendFile(path.join(__dirname,'views/index.html'))
+    res.sendFile(path.join(__dirname,'views/home.html'))
+})
+
+app.get("/projects",(req,res)=>{
+    res.send("pagina proyectos")
 })
 
 app.use(express.static(path.resolve(__dirname,"public")))
 
-app.listen(3000, () => {
-    console.log("servidor corriendo con exito en el puerto 3000");
-})
+let hostVersion = false
+
+if (hostVersion){
+    app.listen(80, () => {
+        console.log("servidor corriendo con exito en el la ip configurada");
+    })
+}else{
+    app.listen(3000, () => {
+        console.log("servidor corriendo con exito en el puerto 3000");
+    })
+}
+
