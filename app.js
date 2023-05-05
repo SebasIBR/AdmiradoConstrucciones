07a454e8f9a98695262d9866d07cd06618330129
SebasIBR <negraoo477@gmail.com>
2023-05-05 18:44:13 -0500

    retornando proyecto

--- app.js
+++ app.js
@@ -2,23 +2,23 @@ const express = require('express');
 const app = express();
 const path = require('path')
 
-app.get("/",(req,res) => {
-    res.sendFile(path.join(__dirname,'views/home.html'))
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/home.html'))
 })
 
-app.get("/projects",(req,res)=>{
+app.get("/projects", (req, res) => {
     res.send("pagina proyectos")
 })
 
-app.use(express.static(path.resolve(__dirname,"public")))
+app.use(express.static(path.resolve(__dirname, "public")))
 
 let hostVersion = false
 
-if (hostVersion){
+if (hostVersion) {
     app.listen(80, () => {
         console.log("servidor corriendo con exito en el la ip configurada");
     })
-}else{
+} else {
     app.listen(3000, () => {
         console.log("servidor corriendo con exito en el puerto 3000");
     })
